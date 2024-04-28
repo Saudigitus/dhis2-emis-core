b48a406eeea11595504407935a9c14d1203390aa
CelsoDeCarvalho <celsomomade361@gmail.com>
2024-04-28 12:10:01 +0200

    Setup school calendar app

---
+++ src/pages/generalDetails/GeneralDetails.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GeneralDetails() {
+  return (
+    <div>GeneralDetails</div>
+  )
+}
+
+export default GeneralDetails
---
+++ src/pages/nonSchoolDays/NonSchoolDays.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NonSchoolDays = () => {
+  return (
+    <div>NonSchoolDays</div>
+  )
+}
+
+export default NonSchoolDays
